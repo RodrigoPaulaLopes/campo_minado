63bce039eab7910c92a94ec3a517faad7ef477fb
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2025-02-01 21:38:12 -0300

    create a project

---
+++ src/test/java/Main.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Main {
+    @Test
+    public void main(){
+        var result = 1 + 1;
+        assertEquals(result, 2);
+    }
+}
